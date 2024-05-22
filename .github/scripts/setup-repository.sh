e0378e00322154e94f9ac23f6789517b63bea3e8
Radek Bartoň <radek.barton@microsoft.com>
2024-05-22 13:42:24 +0200

    Refactor build of individual packages to reusable workflow

--- .github/scripts/setup-repository.sh
+++ .github/scripts/setup-repository.sh
@@ -4,7 +4,7 @@ set -e # exit on error
 set -x # echo on
 set -o pipefail # fail of any command in pipeline is an error
 
-pacman -Syu --noconfirm
+pacman -Syuu --noconfirm
 
 # Add WoArm64 custom repository.
 REPO='[woarm64]
@@ -15,4 +15,3 @@ SigLevel = Optional
 echo -e "$REPO$(cat /etc/pacman.conf)" > /etc/pacman.conf
 
 pacman -Sy --noconfirm
-pacman -S mingw-w64-cross-gcc --noconfirm
