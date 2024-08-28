e5dd81742dd8d47f522e8855b428444d8822c94e
Radek Bartoň <radek.barton@microsoft.com>
2024-08-28 15:09:19 +0200

    Update cross-compilation toolchain to the new upstream packages

--- .github/scripts/pthread-headers-hack-after.sh
+++ .github/scripts/pthread-headers-hack-after.sh
@@ -4,7 +4,7 @@ set -e # exit on error
 set -x # echo on
 set -o pipefail # fail of any command in pipeline is an error
 
-pacman -R --noconfirm mingw-w64-cross-winpthreads || true
+pacman -R --noconfirm mingw-w64-cross-mingw64-winpthreads || true
 rm -rf /opt/aarch64-w64-mingw32/include/pthread_signal.h
 rm -rf /opt/aarch64-w64-mingw32/include/pthread_unistd.h
 rm -rf /opt/aarch64-w64-mingw32/include/pthread_time.h
--- .github/scripts/pthread-headers-hack-before.sh
+++ .github/scripts/pthread-headers-hack-before.sh
@@ -4,7 +4,7 @@ set -e # exit on error
 set -x # echo on
 set -o pipefail # fail of any command in pipeline is an error
 
-pacman -S --noconfirm mingw-w64-cross-winpthreads
+pacman -S --noconfirm mingw-w64-cross-mingw64-winpthreads
 cp /opt/x86_64-w64-mingw32/include/pthread_signal.h /opt/aarch64-w64-mingw32/include/
 cp /opt/x86_64-w64-mingw32/include/pthread_unistd.h /opt/aarch64-w64-mingw32/include/
 cp /opt/x86_64-w64-mingw32/include/pthread_time.h /opt/aarch64-w64-mingw32/include/
