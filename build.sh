916a5c89e567ee271463528649f0b545c1f02fda
Radek Bartoň <radek.barton@microsoft.com>
2024-09-11 08:50:46 +0200

    Update cross-compilation toolchain to the new upstream packages (#16)

--- build.sh
+++ build.sh
@@ -10,64 +10,65 @@ if [ "$CLEAN_BUILD" = 1 ] ; then
   MAKEPKG_OPTIONS="$MAKEPKG_OPTIONS --cleanbuild"
 fi
 
-pacman -R --noconfirm mingw-w64-cross-gcc || true
-pacman -R --noconfirm mingw-w64-cross-winpthreads || true
-pacman -R --noconfirm mingw-w64-cross-crt || true
-pacman -R --noconfirm mingw-w64-cross-windows-default-manifest || true
-pacman -R --noconfirm mingw-w64-cross-gcc-stage1 || true
-pacman -R --noconfirm mingw-w64-cross-binutils || true
-pacman -R --noconfirm mingw-w64-cross-headers || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-zlib || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-gcc || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-winpthreads || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-crt || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-windows-default-manifest || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-gcc-stage1 || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-binutils || true
+pacman -R --noconfirm mingw-w64-cross-mingwarm64-headers || true
 
 pacman -S --noconfirm base-devel
 
-echo "::group::Build mingw-w64-cross-headers"
-  pushd ../MSYS2-packages/mingw-w64-cross-headers
+echo "::group::Build mingw-w64-cross-mingwarm64-headers"
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-headers
     makepkg $MAKEPKG_OPTIONS
   popd
 echo "::endgroup::"
 
-echo "::group::Build mingw-w64-cross-binutils"
-  pushd ../MSYS2-packages/mingw-w64-cross-binutils
+echo "::group::Build mingw-w64-cross-mingwarm64-binutils"
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-binutils
     makepkg $MAKEPKG_OPTIONS
   popd
 echo "::endgroup::"
 
-echo "::group::Build mingw-w64-cross-gcc-stage1"
-  pushd ../MSYS2-packages/mingw-w64-cross-gcc-stage1
+echo "::group::Build mingw-w64-cross-mingwarm64-gcc-stage1"
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-gcc-stage1
     makepkg $MAKEPKG_OPTIONS
   popd
 echo "::endgroup::"
 
-echo "::group::Build mingw-w64-cross-windows-default-manifest"
-  pushd ../MSYS2-packages/mingw-w64-cross-windows-default-manifest
+echo "::group::Build mingw-w64-cross-mingwarm64-windows-default-manifest"
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-windows-default-manifest
     makepkg $MAKEPKG_OPTIONS
   popd
 echo "::endgroup::"
 
-echo "::group::Build mingw-w64-cross-crt"
+echo "::group::Build mingw-w64-cross-mingwarm64-crt"
   .github/scripts/pthread-headers-hack-before.sh
-  pushd ../MSYS2-packages/mingw-w64-cross-crt
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-crt
     makepkg $MAKEPKG_OPTIONS
   popd
   .github/scripts/pthread-headers-hack-after.sh
 echo "::endgroup::"
 
-echo "::group::Build mingw-w64-cross-winpthreads"
-  pushd ../MSYS2-packages/mingw-w64-cross-winpthreads
+echo "::group::Build mingw-w64-cross-mingwarm64-winpthreads"
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-winpthreads
     makepkg $MAKEPKG_OPTIONS
   popd
 echo "::endgroup::"
 
-echo "::group::Build mingw-w64-cross-gcc"
-  pushd ../MSYS2-packages/mingw-w64-cross-gcc
+echo "::group::Build mingw-w64-cross-mingwarm64-gcc"
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-gcc
     makepkg ${MAKEPKG_OPTIONS//--install/}
-    pacman -R --noconfirm mingw-w64-cross-gcc-stage1 || true
+    pacman -R --noconfirm mingw-w64-cross-mingwarm64-gcc-stage1 || true
     pacman -U --noconfirm *.pkg.tar.zst
   popd
 echo "::endgroup::"
 
-echo "::group::Build mingw-w64-cross-zlib"
-  pushd ../MSYS2-packages/mingw-w64-cross-zlib
+echo "::group::Build mingw-w64-cross-mingwarm64-zlib"
+  pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-zlib
     makepkg $MAKEPKG_OPTIONS --skippgpcheck
   popd
 echo "::endgroup::"
