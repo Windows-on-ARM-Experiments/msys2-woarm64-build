c6accd6fc302794e40f60546705cd35cef6c8a9a
Radek Bartoň <blackhex@post.cz>
2024-09-29 11:00:31 +0200

    Add build of `mingw-w64-cross-mingwarm64-winpthreads-stage1` package to `mingw-cross-toolchain.yml`

--- build.sh
+++ build.sh
@@ -3,23 +3,29 @@
 set -e # exit on error
 set -x # echo on
 
+if [[ "$MSYSTEM" != "MSYS" ]]; then
+  echo "This script must be run in the MSYS2 MSYS shell"
+  exit 1
+fi
+
 CLEAN_BUILD=${CLEAN_BUILD:-0}
 
 MAKEPKG_OPTIONS="--syncdeps --rmdeps --noconfirm --noprogressbar --nocheck --force --install"
-if [ "$CLEAN_BUILD" = 1 ] ; then
+if [[ "$CLEAN_BUILD" = 1 ]] ; then
   MAKEPKG_OPTIONS="$MAKEPKG_OPTIONS --cleanbuild"
 fi
 
 pacman -R --noconfirm mingw-w64-cross-mingwarm64-zlib || true
-pacman -R --noconfirm mingw-w64-cross-mingwarm64-gcc || true
-pacman -R --noconfirm mingw-w64-cross-mingwarm64-winpthreads || true
-pacman -R --noconfirm mingw-w64-cross-mingwarm64-crt || true
-pacman -R --noconfirm mingw-w64-cross-mingwarm64-windows-default-manifest || true
+pacman -R --noconfirm mingw-w64-cross-gcc mingw-w64-cross-mingwarm64-gcc || true
+pacman -R --noconfirm mingw-w64-cross-cross-winpthreads mingw-w64-cross-mingwarm64-winpthreads || true
+pacman -R --noconfirm mingw-w64-cross-cross-crt mingw-w64-cross-mingwarm64-crt || true
+pacman -R --noconfirm mingw-w64-cross-windows-default-manifest mingw-w64-cross-mingwarm64-windows-default-manifest || true
 pacman -R --noconfirm mingw-w64-cross-mingwarm64-gcc-stage1 || true
-pacman -R --noconfirm mingw-w64-cross-mingwarm64-binutils || true
-pacman -R --noconfirm mingw-w64-cross-mingwarm64-headers || true
+pacman -R --noconfirm mingw-w64-cross-binutils mingw-w64-cross-mingwarm64-binutils || true
+pacman -R --noconfirm mingw-w64-cross-headers mingw-w64-cross-mingwarm64-headers || true
 
-pacman -S --noconfirm base-devel
+pacman -S --noconfirm \
+  base-devel
 
 echo "::group::Build mingw-w64-cross-mingwarm64-headers"
   pushd ../MSYS2-packages/mingw-w64-cross-mingwarm64-headers
